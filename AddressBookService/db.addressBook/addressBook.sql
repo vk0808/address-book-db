010c7a78711c0356f88b9dacb2b98780928e967b
vk0808 <vk4765712@gmail.com>
2021-09-07 00:17:21 +0530

    feat: create table

--- AddressBookService/db.addressBook/addressBook.sql
+++ AddressBookService/db.addressBook/addressBook.sql
@@ -9,3 +9,23 @@ EXEC sp_databases
 
 -- To go to the database created
 USE address_book
+
+---------------------------------------------------------------
+
+-- uc-2
+-- To go to the database created
+USE address_book
+
+-- To create a table
+CREATE TABLE address(
+	BookID INT IDENTITY(1, 1) PRIMARY KEY,
+	FirstName VARCHAR(255)  NOT NULL,
+	LastName VARCHAR(255)  NOT NULL,
+	Address VARCHAR(250) NOT NULL,
+	City VARCHAR(100) NOT NULL,
+	State VARCHAR(100) NOT NULL,
+	Zip VARCHAR(6) NOT NULL,
+	PhoneNumber VARCHAR(12) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+);
+
