35e07aa4fbda07fbcf5c5b8fb3ac6fd9922af6fd
vk0808 <vk4765712@gmail.com>
2021-09-07 00:10:18 +0530

    feat: create database

---
+++ AddressBookService/db.addressBook/addressBook.sql
@@ -0,0 +1,11 @@
+-- uc-1
+
+-- To Create a database
+CREATE DATABASE address_book
+
+-- To see the DB created
+EXEC sp_databases
+--SELECT * FROM sys.databases;
+
+-- To go to the database created
+USE address_book
