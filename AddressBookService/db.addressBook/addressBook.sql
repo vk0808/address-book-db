1db2ac8581c8a0ecf4d0c28776eba2d4dce8dd3f
vk0808 <vk4765712@gmail.com>
2021-09-07 12:27:40 +0530

    feat: implement er diagram

--- AddressBookService/db.addressBook/addressBook.sql
+++ AddressBookService/db.addressBook/addressBook.sql
@@ -126,3 +126,41 @@ INSERT INTO address(FirstName, LastName, Address, City, State, Zip, PhoneNumber,
 VALUES
 ('Anand','Kumar','#22 !st Cross','Tinsukia','Assam',785575,'8748892361','anand@gmail.com','Book2','Family'),
 ('Anand','Kumar','#22 !st Cross','Tinsukia','Assam',785575,'8748892361','anand@gmail.com','Book1','Friends');
+
+---------------------------------------------------------------
+
+-- uc-12
+-- To go to the database created
+USE address_book
+
+-- To create table
+CREATE TABLE Person
+(
+	PersonID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	FirstName VARCHAR(50),
+	LastName VARCHAR(50),
+	Email VARCHAR(50),
+	PhoneNumber VARCHAR(50),
+);
+
+CREATE TABLE Addresses
+(
+	AddressID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	PersonID INT FOREIGN KEY REFERENCES Person(PersonID),
+	City VARCHAR(50),
+	State VARCHAR(50),
+	Zip INT,
+);
+
+CREATE TABLE BookTypes
+(
+	BookID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	BookName VARCHAR(50),
+	BookType VARCHAR(50),
+);
+
+CREATE TABLE Person_AddressBook
+(
+	PersonID INT FOREIGN KEY REFERENCES Person(PersonID),
+	BookID INT FOREIGN KEY REFERENCES BookTypes(BookID)
+);
