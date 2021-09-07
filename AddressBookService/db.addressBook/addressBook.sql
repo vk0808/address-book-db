586bc0a122a33ca2fa04e2745b2dfe173c38865a
vk0808 <vk4765712@gmail.com>
2021-09-07 10:00:17 +0530

    feat: retrieve records

--- AddressBookService/db.addressBook/addressBook.sql
+++ AddressBookService/db.addressBook/addressBook.sql
@@ -38,11 +38,11 @@ USE address_book
 -- To insert into the table created
 INSERT INTO address(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email) 
 VALUES
-('Rahul ','Gowda','2nd cros Rajaji Nagar','Banglore','Karnataka','58082','8123351458','rahul@gmail.com'),
-('Ravi ','Kumar','3nd cros Jaya Nagar','Banglore','Gujarat','587682','934747358','ravi@gmail.com'),
-('Asha','Kumari','4nd cros Indira Nagar','Banglore','MH','80534','1234567896','asha@gmail.com'),
-('Porvi','Shetty','2nd cros Rajaji Nagar','Banglore','Andra','80453','0987654321','porvi@gmail.com'),
-('Raja','M','2nd cros Rajaji Nagar','Gadag','Andra','80453','0987654321','raja@gmail.com');
+('Rahul ','Gowda','2nd cross Ramnagar Nagar','Mysore','Karnataka','580082','8123351458','rahul@gmail.com'),
+('Ravi ','Kumar','3nd cross','Bangalore','Karnataka','800078','934747358','ravi@gmail.com'),
+('Asha','Kumari','4nd cross Indira Nagar','Bangalore','Karnataka','580003','1234567896','asha@gmail.com'),
+('Porvi','Shetty','2nd cross','Mandya','Karnataka','560078','0987654321','porvi@gmail.com'),
+('Raja','M','2nd cross Rajaji Nagar','Gadag','Karnataka','570006','0987654321','raja@gmail.com');
 
 ---------------------------------------------------------------
 
@@ -62,3 +62,12 @@ USE address_book
 
 -- To delete particular records from table
 DELETE FROM address WHERE FirstName='Asha'
+
+---------------------------------------------------------------
+
+-- uc-6
+-- To go to the database created
+USE address_book
+
+-- To retrieve specific records
+SELECT FirstName FROM address WHERE City = 'Bangalore' OR State = 'Karnataka'
